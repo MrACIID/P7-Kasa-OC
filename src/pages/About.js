56283c8efd8171443cfd863229bf49487ae8eca4
unknown <acidddropdesign@gmail.com>
2022-10-13 15:55:00 +0200

    initialisation react app/router

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, rem
+        voluptatibus, deleniti consequatur culpa quod libero vel accusamus
+        nostrum obcaecati aspernatur non, magnam rerum quos facere harum enim
+        provident quia.
+      </p>
+    </div>
+  );
+};
+
+export default About;
