87ff8849361b0ab750b50d56e1676bcb8327423f
unknown <acidddropdesign@gmail.com>
2022-10-18 14:00:48 +0200

    banner terminé

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <h1>ACCUEIL</h1>
+      <Banner />
     </div>
   );
 };
