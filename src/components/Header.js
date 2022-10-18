d9e9a4cca14b4f1d5b4cbcfcf7419a670c97e844
unknown <acidddropdesign@gmail.com>
2022-10-18 12:34:35 +0200

    header terminé

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import logo from "../styles/assets/logos/logo192.png";
 
 const Header = () => {
   return (
-    <div>
+    <div className="header">
+      <div className="logoheader">
+        <Link to="/">
+          <img src={logo} alt="logo kasa" />
+        </Link>
+      </div>
+
       <div className="navigation">
         <ul>
-          <NavLink to="/">
+          <NavLink to="/" end>
             <li>Accueil</li>
           </NavLink>
           <NavLink to="/about">
