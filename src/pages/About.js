799b73b5ac7844ecc72ffe55cabffb6070fef291
unknown <acidddropdesign@gmail.com>
2022-10-25 15:47:24 +0200

    collapse page a propos finis

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import Collapse from "../components/Collapse";
 import bannerimg from "../styles/assets/bannerabout.png";
 
 const About = () => {
@@ -8,14 +9,30 @@ const About = () => {
     <div>
       <Header />
       <Banner src={bannerimg} alt={"bannière montagnes enneigé"} text={""} />
-      <h1>A propos</h1>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, rem
-        voluptatibus, deleniti consequatur culpa quod libero vel accusamus
-        nostrum obcaecati aspernatur non, magnam rerum quos facere harum enim
-        provident quia.
-      </p>
+      <Collapse
+        text={"Fiabilité"}
+        content={
+          "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+        }
+      />
+      <Collapse
+        text={"Respect"}
+        content={
+          "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        }
+      />
+      <Collapse
+        text={"Service"}
+        content={
+          "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+        }
+      />
+      <Collapse
+        text={"Sécurité"}
+        content={
+          "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        }
+      />
     </div>
   );
 };
