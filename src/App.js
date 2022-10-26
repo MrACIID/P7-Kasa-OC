3431e166d17b2c78d3dde5f257c92275ca4f774f
unknown <acidddropdesign@gmail.com>
2022-10-26 16:03:49 +0200

    début fiche logement

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Errorpage from "./pages/Errorpage";
 import Home from "./pages/Home";
+import Fichelogement from "./pages/Fichelogement";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Errorpage />} />
+        <Route path="/logement/:id" element={<Fichelogement />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Card = ({ logement }) => {
   return (
     <li className="card">
-      <img src={logement.cover} alt="logement" />
-      <p>{logement.title}</p>
+      <Link to={"/logement/" + logement.id}>
+        <img src={logement.cover} alt="logement" />
+        <p>{logement.title}</p>
+      </Link>
     </li>
   );
 };
